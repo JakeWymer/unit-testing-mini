ee456cad777dd7912e221aca3349d1e8adf41569
Jake Wymer <jakeandrewwymer@gmail.com>
2018-08-21 14:58:33 -0500

    add remaining functions and tests

--- functions.js
+++ functions.js
@@ -10,8 +10,23 @@ const add = (x, y) => {
   return x + y;
 }
 
+const subtract = (x, y) => {
+  return x - y;
+}
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+const divide = (x, y) => {
+  return x/y;
+}
+
 module.exports = {
   returnTwo,
   greeting,
-  add
+  add,
+  subtract,
+  multiply,
+  divide
 }
--- functions.test.js
+++ functions.test.js
@@ -9,6 +9,21 @@ test("greeting('jake') should return 'Hello, jake'", () => {
   expect( functions.greeting('ashley') ).toEqual( 'Hello, ashley' );
 });
 
-test("add(1, 2) should return 3.", () => {
-  expect( functions.add(1, 2) ).toEqual( 3 );
+
+describe('Math Group', () => {
+  test("add(1, 2) should return 3.", () => {
+    expect( functions.add(1, 2) ).toEqual( 3 );
+  });
+  
+  test("subtract(2, 1) should return 1.", () => {
+    expect( functions.subtract(2, 1) ).toEqual( 1 );
+  });
+
+  test("multiply(1, 2) should return 2.", () => {
+    expect( functions.multiply(1, 2) ).toEqual( 2 );
+  });
+
+  test("divide(10, 5) should return 2.", () => {
+    expect( functions.divide(10, 5) ).toEqual( 2 );
+  });
 });
